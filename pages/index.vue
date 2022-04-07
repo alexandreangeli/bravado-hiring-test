a674d9964d78684d70114250f93be9491ba9f914
Alexandre Angeli <ale.angeli@hotmail.com>
2022-04-07 01:14:36 -0300

    Created get request to fetch users

--- pages/index.vue
+++ pages/index.vue
@@ -19,7 +19,7 @@ import Vue from 'vue'
 import SearchInput from '~/components/SearchInput.vue'
 import UserCard from '~/components/UserCard.vue'
 import QueryParams from '~/constants/QueryParams'
-import usersJson from '~/static/users.json'
+import GetUsersURL from '~/constants/GetUsersURL'
 import IUser from '~/types/IUser'
 
 export default Vue.extend({
@@ -31,7 +31,7 @@ export default Vue.extend({
     allUsers: IUser[]
   } {
     return {
-      allUsers: usersJson,
+      allUsers: [],
     }
   },
 
@@ -52,6 +52,11 @@ export default Vue.extend({
           )
         : this.allUsers
     },
+  },
+
+  async fetch() {
+    const usersResponse = await fetch(GetUsersURL)
+    this.allUsers = await usersResponse.json()
   },
 })
 </script>
