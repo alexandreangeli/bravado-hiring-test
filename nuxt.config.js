b93afadaffb2f8c6378fe4865af67a52fac12e11
Alexandre Angeli <ale.angeli@hotmail.com>
2022-04-07 09:28:36 -0300

    Removed infinite loading plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,7 @@ export default {
   css: ['~/assets/css/main.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{ src: '~/plugins/infiniteloading', ssr: false }],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
